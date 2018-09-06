4f929a75ff5ebe4f145c69606d25f6d8cb6528dd
Carlos Kometter <ckometter@gmail.com>
2018-09-06 11:04:03 -0700

    New AD5791 class.

---
+++ include/AD5791.h
@@ -0,0 +1,47 @@
+// Class for a Analog Devices AD5791 DAC
+// Copyright (C) 2018 Carlos Kometter 
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef AD5791_h_
+#define AD5791_h_
+
+#include "dac.h"
+
+///
+/// AD5791 class
+/// \author Carlos Kometter
+/// \version 0.1
+/// \date 2018
+/// \copyright GNU Public License.
+///
+class AD5791: public Dac {
+ private:
+  uint8_t slot_number_;
+  double full_scale_;
+ public:
+  ///
+  /// Constructor
+  /// \param[in] sync_pin The sync or chip select of the dac chip. Different than spi_bus_config_pin.
+  /// \param[in] spi_bus_config_pin The pin that identifies the bus. More than one dac can share the same pin.
+  /// \param[in] ldac_pin The ldac pin used to update the analog outputs.
+  /// \param[in] slot_number This number is used to identify the chip or board. (default: 0) 
+  /// \param[in] full_scale The full scale of the dac voltage in V units. (default: 10) 
+  ///
+  AD5791(uint8_t sync_pin, uint8_t spi_bus_config_pin,
+         uint8_t ldac_pin, uint8_t slot_number=0, double full_scale=10.0);
+  virtual double BytesToVoltage(spi_utils::Message message);
+  virtual spi_utils::Message SetVoltageMessage(uint8_t channel, double voltage);
+};
+#endif
--- src/AD5791.cpp
+++ src/AD5791.cpp
@@ -1,4 +1,4 @@
-// Class for a Analog Devices AD5764 DAC
+// Class for a Analog Devices AD5791 DAC
 // Copyright (C) 2018 Carlos Kometter 
 //
 // This program is free software: you can redistribute it and/or modify
@@ -14,41 +14,42 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "../include/AD5764.h"
+#include "../include/AD5791.h"
 
-AD5764::AD5764(uint8_t sync_pin, uint8_t spi_bus_config_pin,
+AD5791::AD5791(uint8_t sync_pin, uint8_t spi_bus_config_pin,
 	       uint8_t ldac_pin, uint8_t slot_number, double full_scale)
-  :Dac(sync_pin, spi_bus_config_pin, ldac_pin, 16),
+  :Dac(sync_pin, spi_bus_config_pin, ldac_pin, 20),
    slot_number_(slot_number), full_scale_(full_scale) {
 }
 
-double AD5764::BytesToVoltage(spi_utils::Message message) {
-  byte byte1 = message.msg[1];
-  byte byte2 = message.msg[2];
+double AD5791::BytesToVoltage(spi_utils::Message message) {
+  byte byte1 = message.msg[0];
+  byte byte2 = message.msg[1];
+  byte byte3 = message.msg[2];
   // The conversion below is for two's complement
-  uint16_t decimal = ((uint16_t)((byte1 << 8) | byte2));
+  uint16_t decimal = ((uint16_t)(((((byte1 & 0x15) << 0x8) | byte2) << 0x8) | byte3));
   double voltage;
-  if (decimal > 32767) {
-    voltage = -(65536-decimal)*full_scale_/32768;
+  if (decimal > 524287) {
+    voltage = -(1048576-decimal)*full_scale_/524288;
   } else {
-    voltage = decimal*full_scale_/32767;
+    voltage = decimal*full_scale_/524287;
   }
   return voltage;
 }
 
-spi_utils::Message AD5764::SetVoltageMessage(uint8_t channel, double voltage) {
+spi_utils::Message AD5791::SetVoltageMessage(uint8_t channel, double voltage) {
   uint16_t decimal;
   spi_utils::Message msg;
   msg.block_size = 3;
   msg.n_blocks = 1;
   // The conversion below is for two's complement
   if (voltage < 0) {
-    decimal = voltage*32768/full_scale_ + 65536;
+    decimal = voltage*524288/full_scale_ + 1048576;
   } else {
-    decimal = voltage*32767/full_scale_;
+    decimal = voltage*524287/full_scale_;
   }
   // Check datasheet for details
-  msg.msg[0] = 0x10 + channel;  // Writes to dac register
+  msg.msg[0] = (byte)((decimal >> 0x10) | 0x10);  // Writes to dac register
   msg.msg[1] = ((byte)((decimal >> 0x8) & 0xFF));  // Writes first byte
   msg.msg[2] = ((byte)(decimal & 0xFF));  // Writes second byte
   return msg;
